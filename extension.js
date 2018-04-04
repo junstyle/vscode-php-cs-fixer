330761b38fcce7e02b85bef32aad7b50860748b1
Carlos C Soto <eclipxe13@gmail.com>
2018-04-04 15:12:00 -0500

    do not replace config starting with ~/ in load settings stage

--- extension.js
+++ extension.js
@@ -37,7 +37,7 @@ class PHPCSFixer {
         if (typeof (this.rules) == 'object') {
             this.rules = JSON.stringify(this.rules);
         }
-        this.config = config.get('config', '.php_cs').replace(/^~\//, os.homedir() + '/');
+        this.config = config.get('config', '.php_cs');
         this.formatHtml = config.get('formatHtml', false);
         this.documentFormattingProvider = config.get('documentFormattingProvider', true);
         this.allowRisky = config.get('allowRisky', false);
