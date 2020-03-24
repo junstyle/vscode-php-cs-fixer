923ff7c7a0324c6b37a137f101b5b54dc21c4612
junstyle <junstyle@git>
2020-03-24 10:11:59 +0800

    bug fix and optimize

--- download-phar.js
+++ download-phar.js
@@ -1,4 +1,4 @@
-let download = require('download');
-download('https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar', __dirname, { 'filename': 'php-cs-fixer.phar' }).then(() => {
-	console.log('download php-cs-fixer.phar successfully.');
-});
+const { DownloaderHelper } = require('node-downloader-helper');
+let dl = new DownloaderHelper('https://cs.symfony.com/download/php-cs-fixer-v2.phar', __dirname, { 'fileName': 'php-cs-fixer.phar', 'override': true });
+dl.on('end', () => console.log('download php-cs-fixer.phar successfully.'));
+dl.start();
