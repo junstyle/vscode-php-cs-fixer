2b6d28e8780bfe03156dc8f4edd7a45e48d24558
junstyle <junstyle@git>
2020-03-24 10:19:49 +0800

    bug fix and optimize

--- download-phar.js
+++ download-phar.js
@@ -1,4 +1,7 @@
 const { DownloaderHelper } = require('node-downloader-helper');
 let dl = new DownloaderHelper('https://cs.symfony.com/download/php-cs-fixer-v2.phar', __dirname, { 'fileName': 'php-cs-fixer.phar', 'override': true });
 dl.on('end', () => console.log('download php-cs-fixer.phar successfully.'));
+dl.on('progress', stat => {
+	console.log('\rdownloading php-cs-fixer.phar: ' + stat.progress);
+});
 dl.start();
