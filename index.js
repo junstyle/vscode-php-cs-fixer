e1e2a0b2c59f5d7d8650fb407009108c16f2c8fa
junstyle <junstyle@git>
2020-04-09 14:04:48 +0800

    webpack js files

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 if (fs.existsSync(__dirname + '/extension_pack.js'))
 	module.exports = require(__dirname + '/extension_pack');
 else
-	module.exports = require(__dirname + '/extension');
+	module.exports = require(__dirname + '/extension'); // for development
